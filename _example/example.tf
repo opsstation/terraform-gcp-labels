2797880ba214991c20b6745848eca0c6a6255ca5
Sohan Yadav <sohanyadavdevops@gmail.com>
2024-02-12 15:35:12 +0530

    first commit

---
+++ _example/example.tf
@@ -0,0 +1,9 @@
+module "labels" {
+  source      = "./../"
+  name        = "labels"
+  environment = "test"
+  label_order = ["name", "environment"]
+  attributes  = ["private"]
+  extra_tags = {
+  }
+}
